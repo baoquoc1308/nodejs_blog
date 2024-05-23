230f120dbbf35c691cbddf5e76db47ae901a5174
Hồ Bảo Quốc <baoquochoa2@gmail.com>
2024-05-23 14:49:51 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+var methodOverride = require('method-override')
+const handlebars = require('express-handlebars')
+
+const route = require('./routes')
+const db = require('./config/db')
+
+// Connect to DB
+db.connect()
+
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
+app.use(express.json())
+app.use(methodOverride('_method'))
+
+//HTTP logger
+// app.use(morgan('combined'))
+
+//Template engine
+app.engine(
+  'hbs',
+  handlebars.engine({
+    extname: '.hbs',
+    helpers: {
+      sum: (a, b) => a + b,
+    },
+  })
+)
+app.set('view engine', 'hbs')
+
+app.set('views', path.join(__dirname, 'resources', 'views'))
+
+// routes init
+
+route(app)
+
+// 127.0.0.1 - localhost:3000
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
